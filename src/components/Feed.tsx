6a10833a1decfd7ef7b029583cb21adc7fca9eab
Naoki-Kameyama <atukan0930@gmail.com>
2022-02-14 05:56:28 +0900

    ツイート機能

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import {auth} from "../firebase"
+import TweetInput from './TweetInput'
+import styles from "./Feed.module.css"
 
 function Feed() {
   return (
-    <div>Feed
+    <div className={styles.feed}>
+      Feed
+      <TweetInput/>
       <button onClick={()=>auth.signOut()}>Logout</button>
     </div>
   )
